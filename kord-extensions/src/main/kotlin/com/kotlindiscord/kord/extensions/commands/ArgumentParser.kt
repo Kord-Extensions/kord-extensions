fd5cda5fe34b6641e2a390938fb0ffd94faf4edd
Gareth Coles <gdude2002@gmail.com>
2020-05-09 14:57:14 +0100

    Lint/document, but things are still early.

--- kord-extensions/src/main/kotlin/com/kotlindiscord/kord/extensions/commands/ArgumentParser.kt
+++ kord-extensions/src/main/kotlin/com/kotlindiscord/kord/extensions/commands/ArgumentParser.kt
@@ -1,7 +1,6 @@
 package com.kotlindiscord.kord.extensions.commands
 
 import com.gitlab.kordlib.common.entity.Snowflake
-import com.gitlab.kordlib.core.entity.Role
 import com.gitlab.kordlib.core.entity.User
 import com.gitlab.kordlib.core.entity.channel.Channel
 import com.kotlindiscord.kord.extensions.ExtensibleBot
@@ -13,9 +12,29 @@ import kotlin.reflect.full.createType
 import kotlin.reflect.full.isSubtypeOf
 import kotlin.reflect.full.primaryConstructor
 
-class ArgumentParser(val bot: ExtensibleBot) {
+/**
+ * Class in charge of converting string arguments for commands into fully-typed data classes.
+ *
+ * You most likely don't need to touch this yourself - it's part of the commands framework.
+ *
+ * @param bot Instance of [ExtensibleBot] we're working with.
+ */
+class ArgumentParser(private val bot: ExtensibleBot) {
+    /** Defined here so we don't have to create it every time we try to parse something. */
     private val listType = List::class.createType(arguments = listOf(KTypeProjection.STAR))
 
+    /**
+     * Given a data class and an array of strings, return an instance of the data class that
+     * has been populated from the array of strings.
+     *
+     * This function needs a lot of work:
+     *     TODO: Raise exception if not data class
+     *     TODO: Think about exception handling for conversions
+     *     TODO: Think about null handling and default arguments
+     *
+     * @param dataclass Data class to parse Strings into.
+     * @param args Array of Strings to parse.
+     */
     suspend fun <T : Any> parse(dataclass: KClass<T>, args: Array<out String>): T? {
         if (!dataclass.isData || dataclass.primaryConstructor == null) {
             TODO("Raise exception here")
@@ -28,13 +47,13 @@ class ArgumentParser(val bot: ExtensibleBot) {
             println("$i, ${element.type}")
             println()
 
-            when {
-                element.type.isSubtypeOf(listType) -> dcArgs[element] = stringsToTypes(
+            if (element.type.isSubtypeOf(listType)) {
+                dcArgs[element] = stringsToTypes(
                     args.sliceArray(i until args.size),
                     element.type.arguments[0].type!!
                 )
-
-                else -> dcArgs[element] = stringToType(args[i], element.type)
+            } else {
+                dcArgs[element] = stringToType(args[i], element.type)
             }
         }
 
@@ -43,7 +62,7 @@ class ArgumentParser(val bot: ExtensibleBot) {
         return dataclass.primaryConstructor?.callBy(dcArgs)
     }
 
-    suspend fun stringToType(string: String, type: KType): Any? {
+    private suspend fun stringToType(string: String, type: KType): Any? {
         return when {
             type.isSubtypeOf(Int::class.createType()) -> string.toInt()
             type.isSubtypeOf(Long::class.createType()) -> string.toLong()
@@ -59,7 +78,7 @@ class ArgumentParser(val bot: ExtensibleBot) {
         }
     }
 
-    suspend fun stringsToTypes(strings: Array<out String>, type: KType): List<Any?> {
+    private suspend fun stringsToTypes(strings: Array<out String>, type: KType): List<Any?> {
         val values: MutableList<Any?> = mutableListOf()
 
         strings.forEach { values.add(stringToType(it, type)) }
