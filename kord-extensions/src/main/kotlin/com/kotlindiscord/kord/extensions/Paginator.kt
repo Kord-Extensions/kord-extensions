03af63c9a343701f270fbc3f6e857aa879769d36
Matteo Bertucci <matteobertucci2004@gmail.com>
2020-05-19 17:27:25 +0200

    Move paginator to kord-extensions

---
+++ kord-extensions/src/main/kotlin/com/kotlindiscord/kord/extensions/Paginator.kt
@@ -0,0 +1,141 @@
+package com.kotlindiscord.kord.extensions
+
+import com.gitlab.kordlib.common.annotation.KordPreview
+import com.gitlab.kordlib.core.behavior.channel.MessageChannelBehavior
+import com.gitlab.kordlib.core.behavior.channel.createEmbed
+import com.gitlab.kordlib.core.behavior.edit
+import com.gitlab.kordlib.core.entity.Message
+import com.gitlab.kordlib.core.entity.ReactionEmoji
+import com.gitlab.kordlib.core.entity.User
+import com.gitlab.kordlib.core.event.message.ReactionAddEvent
+import com.gitlab.kordlib.rest.builder.message.EmbedBuilder
+import kotlinx.coroutines.delay
+import mu.KotlinLogging
+
+private val FIRST_PAGE_EMOJI = ReactionEmoji.Unicode("\u23EE")
+private val LEFT_EMOJI = ReactionEmoji.Unicode("\u2B05")
+private val RIGHT_EMOJI = ReactionEmoji.Unicode("\u27A1")
+private val LAST_PAGE_EMOJI = ReactionEmoji.Unicode("\u23ED")
+private val DELETE_EMOJI = ReactionEmoji.Unicode("\u274C")
+
+private val EMOJIS = arrayOf(FIRST_PAGE_EMOJI, LEFT_EMOJI, RIGHT_EMOJI, LAST_PAGE_EMOJI, DELETE_EMOJI)
+
+private val logger = KotlinLogging.logger {}
+
+/**
+ * Interactive embed with multiple pages using emoji reactions as user inputs.
+ *
+ * NOTE : `.send()` needs to be called in order for the paginator to be displayed.
+ *
+ * @param bot Current instance of the bot.
+ * @param channel Channel to send the embed to.
+ * @param name Title of the embed.
+ * @param pages List of the embed pages.
+ * @param owner Only user capable of interacting with the paginator. Everyone is able to if it is set to null.
+ * @param timeout Milliseconds before automatically deleting the embed. Set to a negative value to disable it.
+ * @param keepEmbed Keep the embed and only remove the reaction when the paginator is destroyed.
+ */
+class Paginator(
+    val bot: ExtensibleBot,
+    val channel: MessageChannelBehavior,
+    val name: String,
+    val pages: List<String>,
+    val owner: User? = null,
+    val timeout: Long = -1L,
+    val keepEmbed: Boolean = false
+) {
+    /** Message containing the embed. **/
+    var message: Message? = null
+    /** Current page of the paginator. **/
+    var currentPage: Int = 0
+    /** Whether the paginator still processes reaction events. **/
+    var doesProcessEvents: Boolean = true
+
+    /** Send the embed to the channel given in the constructor. **/
+    @KordPreview
+    suspend fun send() {
+        val myFooter = EmbedBuilder.Footer()
+        myFooter.text = "Page 1/${pages.size}"
+
+        message = channel.createEmbed {
+            title = name
+            description = pages[0]
+            footer = myFooter
+        }
+
+        EMOJIS.forEach { message!!.addReaction(it) }
+
+        while (true) {
+            val event = bot.kord.waitFor<ReactionAddEvent>(timeout = timeout) {
+                message.id == this.messageId &&
+                this.userId != bot.kord.selfId &&
+                (owner?.id == this.userId || owner == null) &&
+                doesProcessEvents
+            } ?: break
+
+            processEvent(event)
+        }
+
+        if (timeout > 0) {
+            delay(timeout)
+            destroy()
+        }
+    }
+
+    /**
+     * Paginator [ReactionAddEvent] handler.
+     *
+     * @param event [ReactionAddEvent] to process.
+     */
+    suspend fun processEvent(event: ReactionAddEvent) {
+        logger.debug { "Paginator received emoji ${event.emoji.name}" }
+        event.message.deleteReaction(event.userId, event.emoji)
+
+        when (event.emoji.name) {
+            FIRST_PAGE_EMOJI.name    -> goToPage(0)
+            LEFT_EMOJI.name          -> goToPage(currentPage - 1)
+            RIGHT_EMOJI.name         -> goToPage(currentPage + 1)
+            LAST_PAGE_EMOJI.name     -> goToPage(pages.size - 1)
+            DELETE_EMOJI.name        -> destroy()
+            else                     -> return
+        }
+    }
+
+    /** Display the provided page number.
+     *
+     * @param page Page number to display.
+     */
+    suspend fun goToPage(page: Int) {
+        if (page == currentPage) {
+            return
+        }
+        if (page < 0 || page > pages.size - 1) {
+            return
+        }
+
+        currentPage = page
+
+        val myFooter = EmbedBuilder.Footer()
+        myFooter.text = "Page ${page + 1}/${pages.size}"
+
+        message?.edit { embed {
+            title = name
+            description = pages[page]
+            footer = myFooter
+        } }
+    }
+
+    /** Destroy the paginator.
+     *
+     * This will make it stops receive [ReactionAddEvent] and will delete the embed if `keepEmbed` is set to true,
+     * or will delete all the reactions if it is set to false.
+     */
+    suspend fun destroy() {
+        if (!keepEmbed) {
+            message?.delete()
+        } else {
+            message?.deleteAllReactions()
+        }
+        doesProcessEvents = false
+    }
+}
