50f25a759b8ca16dedd387045728a1364f379848
Matteo Bertucci <matteobertucci2004@gmail.com>
2020-07-30 13:49:02 +0200

    Add Scheduler class (#8)
    * Scheduler basics
    
    * Add more functionalities to the scheduler.
    
    * Use custom scope inside the scheduler
    
    According to the docs, using GlobalScope is a bad pratice.
    
    * Move to UUIDs, throw exception instead of return boolean
    
    Co-authored-by: Gareth Coles <gdude2002@gmail.com>

---
+++ kord-extensions/src/main/kotlin/com/kotlindiscord/kord/extensions/checks/utils/Scheduler.kt
@@ -0,0 +1,109 @@
+package com.kotlindiscord.kord.extensions.checks.utils
+
+import kotlinx.coroutines.*
+import mu.KotlinLogging
+import java.util.*
+
+private val logger = KotlinLogging.logger { }
+
+/**
+ * Class in charge of providing scheduling functions.
+ */
+open class Scheduler {
+    private val jobMap: MutableMap<UUID, Job> = mutableMapOf()
+
+    private val scope = GlobalScope
+    private val finishTask = CancellationException()
+
+    /**
+     * Schedule a new task, with a randomly generated ID.
+     *
+     * @param delay How long in milliseconds to wait before executing the callback.
+     * @param data Arbitrary data to be passed to the callback.
+     * @param callback Function to be executed.
+     *
+     * @return Return true on success, false otherwise.
+     */
+    fun <T> schedule(delay: Long, data: T?, callback: (T?) -> Unit): UUID {
+        val uuid = UUID.randomUUID()
+
+        schedule(uuid, delay, data, callback)
+
+        return uuid
+    }
+
+    /**
+     * Schedule a new task.
+     *
+     * @param id The ID of the task.
+     * @param delay How long in milliseconds to wait before executing the callback.
+     * @param data Arbitrary data to be passed to the callback.
+     * @param callback Function to be executed.
+     *
+     * @return Return true on success, false otherwise.
+     */
+    fun <T> schedule(id: UUID, delay: Long, data: T?, callback: (T?) -> Unit) {
+        logger.debug { "Scheduling task $id" }
+
+        if (id in jobMap) {
+            throw IllegalArgumentException("Duplicate ID: $id")
+        }
+
+        val job = scope.launch {
+            delay(delay)
+            callback(data)
+        }
+
+        job.invokeOnCompletion {
+            if (it == finishTask) {
+                callback(data)
+            }
+        }
+
+        jobMap[id] = job
+    }
+
+    /**
+     * Immediately execute the callback of the provided task.
+     *
+     * @param id ID of the targeted task.
+     */
+    fun finishJob(id: UUID) {
+        logger.debug { "Finishing task $id" }
+
+        jobMap[id]?.cancel(finishTask)
+    }
+
+    /**
+     * Cancel the provided task.
+     *
+     * @param id ID of the targeted task.
+     */
+    fun cancelJob(id: UUID) {
+        logger.debug { "Canceling task $id" }
+
+        jobMap[id]?.cancel()
+    }
+
+    /**
+     * Immediately execute the callback of all the tasks belonging to this scheduler.
+     */
+    fun finishAll() {
+        logger.debug { "Finishing all tasks." }
+
+        for (id in jobMap.keys) {
+            finishJob(id)
+        }
+    }
+
+    /**
+     * Cancel all the tasks belonging to this scheduler.
+     */
+    fun cancelAll() {
+        logger.debug { "Canceling all tasks." }
+
+        for (id in jobMap.keys) {
+            cancelJob(id)
+        }
+    }
+}
