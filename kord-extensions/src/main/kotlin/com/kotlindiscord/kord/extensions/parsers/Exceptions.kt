4202fcb509bc5b081b3fb0d9e667debc50974338
ks129 <45097959+ks129@users.noreply.github.com>
2020-08-27 11:35:39 +0300

    Create base exception for parsers and throw custom exception when invalid time unit provided to parser (#9)
    * Replace [key] with `getValue` to throw better error
    
    * Add InvalidTimeUnitException for duration parsers
    
    * Throw custom exception when invalid time unit provided
    
    * Move defining chronoUnit outside of try block
    
    * Create base exception for parsers and move invalid time unit exception
    
    * Use elvis operator instead full try-except
    
    * Remove trailing spaces from duration parser
    
    * Fix linting errors

---
+++ kord-extensions/src/main/kotlin/com/kotlindiscord/kord/extensions/parsers/Exceptions.kt
@@ -0,0 +1,17 @@
+package com.kotlindiscord.kord.extensions.parsers
+
+import com.kotlindiscord.kord.extensions.ExtensionsException
+
+/**
+ * A base exception class for parsers.
+ */
+open class BaseParserException : ExtensionsException()
+
+/**
+ * Throws when invalid time unit given to duration parser.
+ *
+ * @param unit Invalid unit.
+ */
+class InvalidTimeUnitException(var unit: String) : BaseParserException() {
+    override fun toString(): String = "Invalid time unit provided: $unit"
+}
