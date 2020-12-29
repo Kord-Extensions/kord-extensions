eec694a96d23edc415c4cd8f4c0af06f3ec19f2e
Gareth Coles <gdude2002@gmail.com>
2020-12-29 12:35:42 +0000

    Debug logging for Koin in test bot

--- kord-extensions/src/test/kotlin/com/kotlindiscord/kord/extensions/test/bot/Bot.kt
+++ kord-extensions/src/test/kotlin/com/kotlindiscord/kord/extensions/test/bot/Bot.kt
@@ -1,9 +1,14 @@
 package com.kotlindiscord.kord.extensions.test.bot
 
 import com.kotlindiscord.kord.extensions.ExtensibleBot
+import org.koin.core.logger.Level
 
 suspend fun main() {
-    val bot = ExtensibleBot(System.getenv("TOKEN"), "!")
+    val bot = ExtensibleBot(
+        System.getenv("TOKEN"),
+        "!",
+        koinLogLevel = Level.DEBUG
+    )
 
     bot.addExtension(TestExtension::class)
 
