9973ece97fd0afe6a6b9cbb93b776220a24f61bb
Gareth Coles <gdude2002@gmail.com>
2021-05-14 12:17:16 +0100

    IJ failed to notice a file was modified for commit!

--- kord-extensions/src/main/kotlin/com/kotlindiscord/kord/extensions/utils/_Koin.kt
+++ kord-extensions/src/main/kotlin/com/kotlindiscord/kord/extensions/utils/_Koin.kt
@@ -1,18 +1,24 @@
 package com.kotlindiscord.kord.extensions.utils
 
 import org.koin.core.Koin
+import org.koin.core.context.loadKoinModules
 import org.koin.core.module.Module
 import org.koin.dsl.ModuleDeclaration
+import org.koin.dsl.module
+import org.koin.mp.KoinPlatformTools
 
 /** Wrapper for [org.koin.dsl.module] that immediately loads the module for the current [Koin] instance. **/
-public fun Koin.module(
+public fun loadModule(
     createdAtStart: Boolean = false,
     override: Boolean = false,
     moduleDeclaration: ModuleDeclaration
 ): Module {
-    val module = org.koin.dsl.module(createdAtStart, override, moduleDeclaration)
+    val moduleObj = module(createdAtStart, override, moduleDeclaration)
 
-    loadModules(listOf(module))
+    loadKoinModules(moduleObj)
 
-    return module
+    return moduleObj
 }
+
+/** Retrieve the current [Koin] instance. **/
+public fun getKoin(): Koin = KoinPlatformTools.defaultContext().get()
