25c2617dec0a2678bbe9f2f4f2210e480241a54f
Gareth Coles <gdude2002@gmail.com>
2021-05-21 12:18:08 +0100

    Update supported locales

--- java-time/src/test/kotlin/com/kotlindiscord/kord/extensions/test/bot/Bot.kt
+++ java-time/src/test/kotlin/com/kotlindiscord/kord/extensions/test/bot/Bot.kt
@@ -1,9 +1,9 @@
 package com.kotlindiscord.kord.extensions.test.bot
 
 import com.kotlindiscord.kord.extensions.ExtensibleBot
+import com.kotlindiscord.kord.extensions.i18n.SupportedLocales
 import com.kotlindiscord.kord.extensions.utils.env
 import org.koin.core.logger.Level
-import java.util.*
 
 suspend fun main() {
     val bot = ExtensibleBot(env("TOKEN")!!) {
@@ -12,10 +12,11 @@ suspend fun main() {
         i18n {
             localeResolver { guild, channel, user ->
                 when (user?.id?.value) {
-                    242043299022635020 -> Locale.FRANCE
-                    407110650217627658 -> Locale.FRANCE
-                    667552017434017794 -> Locale.SIMPLIFIED_CHINESE
-                    185461862878543872 -> Locale.GERMAN
+                    560515299388948500 -> SupportedLocales.FINNISH
+                    242043299022635020 -> SupportedLocales.FRENCH
+                    407110650217627658 -> SupportedLocales.FRENCH
+                    667552017434017794 -> SupportedLocales.CHINESE_SIMPLIFIED
+                    185461862878543872 -> SupportedLocales.GERMAN
 
                     else -> defaultLocale
                 }
--- kord-extensions/src/main/kotlin/com/kotlindiscord/kord/extensions/i18n/SupportedLocales.kt
+++ kord-extensions/src/main/kotlin/com/kotlindiscord/kord/extensions/i18n/SupportedLocales.kt
@@ -9,9 +9,17 @@ import java.util.*
  * back: https://crowdin.com/project/kordex
  */
 public object SupportedLocales {
+    public val CHINESE_SIMPLIFIED: Locale = Locale("zh", "cn")
     public val ENGLISH: Locale = Locale("en", "gb")
+    public val FINNISH: Locale = Locale("fi", "fi")
+    public val FRENCH: Locale = Locale("fr", "fr")
+    public val GERMAN: Locale = Locale("de", "de")
 
     public val ALL_LOCALES: Map<String, Locale> = mapOf(
+        "zh" to CHINESE_SIMPLIFIED,
         "en" to ENGLISH,
+        "fi" to FINNISH,
+        "fr" to FRENCH,
+        "de" to GERMAN,
     )
 }
