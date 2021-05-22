7420086cb3471d09379eb944bc086bfe8e1eb8e9
Gareth Coles <gdude2002@gmail.com>
2021-05-22 16:36:20 +0100

    Add translation functions to event handlers

--- kord-extensions/src/main/kotlin/com/kotlindiscord/kord/extensions/events/EventHandler.kt
+++ kord-extensions/src/main/kotlin/com/kotlindiscord/kord/extensions/events/EventHandler.kt
@@ -3,6 +3,7 @@ package com.kotlindiscord.kord.extensions.events
 import com.kotlindiscord.kord.extensions.InvalidEventHandlerException
 import com.kotlindiscord.kord.extensions.checks.*
 import com.kotlindiscord.kord.extensions.extensions.Extension
+import com.kotlindiscord.kord.extensions.i18n.TranslationsProvider
 import com.kotlindiscord.kord.extensions.sentry.SentryAdapter
 import com.kotlindiscord.kord.extensions.sentry.tag
 import dev.kord.common.entity.Snowflake
@@ -16,6 +17,7 @@ import mu.KotlinLogging
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
 import java.io.Serializable
+import java.util.*
 import kotlin.reflect.KClass
 
 private val logger = KotlinLogging.logger {}
@@ -37,6 +39,9 @@ public class EventHandler<T : Any>(public val extension: Extension, public val t
     /** Kord instance, backing the ExtensibleBot. **/
     public val kord: Kord by inject()
 
+    /** Translations provider, for retrieving translations. **/
+    public val translationsProvider: TranslationsProvider by inject()
+
     /**
      * @suppress
      */
@@ -51,6 +56,9 @@ public class EventHandler<T : Any>(public val extension: Extension, public val t
      * @suppress This is the job returned by `Kord#on`, which we cancel to stop listening.
      */
     public var job: Job? = null
+
+    /** Cached locale variable, stored and retrieved by [getLocale]. **/
+    public var resolvedLocale: Locale? = null
 
     /**
      * An internal function used to ensure that all of an event handler's required arguments are present.
@@ -213,4 +221,54 @@ public class EventHandler<T : Any>(public val extension: Extension, public val t
             }
         }
     }
+
+    /** Resolve the locale for the given event. **/
+    public suspend fun Event.getLocale(): Locale {
+        var locale: Locale? = resolvedLocale
+
+        if (locale != null) {
+            return locale
+        }
+
+        val guild = guildFor(this)
+        val channel = channelFor(this)
+        val user = userFor(this)
+
+        for (resolver in extension.bot.settings.i18nBuilder.localeResolvers) {
+            val result = resolver(guild, channel, user)
+
+            if (result != null) {
+                locale = result
+                break
+            }
+        }
+
+        resolvedLocale = locale ?: extension.bot.settings.i18nBuilder.defaultLocale
+
+        return resolvedLocale!!
+    }
+
+    /**
+     * Given a translation key and bundle name, return the translation for the locale provided by the bot's configured
+     * locale resolvers.
+     */
+    public suspend fun Event.translate(
+        key: String,
+        bundleName: String?,
+        replacements: Array<Any?> = arrayOf()
+    ): String {
+        val locale = getLocale()
+
+        return translationsProvider.translate(key, locale, bundleName, replacements)
+    }
+
+    /**
+     * Given a translation key and possible replacements,return the translation for the given locale in the
+     * extension's configured bundle, for the locale provided by the bot's configured locale resolvers.
+     */
+    public suspend fun Event.translate(key: String, replacements: Array<Any?> = arrayOf()): String = translate(
+        key,
+        extension.bundle,
+        replacements
+    )
 }
