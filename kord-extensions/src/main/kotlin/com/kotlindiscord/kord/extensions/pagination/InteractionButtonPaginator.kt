2ceac1a0e6f61ccec320d8a6257fc151f66e091e
Gareth Coles <gdude2002@gmail.com>
2021-06-25 17:00:22 +0100

    Button paginators: Now timeout callbacks are run again

--- kord-extensions/src/main/kotlin/com/kotlindiscord/kord/extensions/pagination/InteractionButtonPaginator.kt
+++ kord-extensions/src/main/kotlin/com/kotlindiscord/kord/extensions/pagination/InteractionButtonPaginator.kt
@@ -87,6 +87,8 @@ public class InteractionButtonPaginator(
                 this.components = mutableListOf()
             }
         }
+
+        runTimeoutCallbacks()
     }
 }
 
--- kord-extensions/src/main/kotlin/com/kotlindiscord/kord/extensions/pagination/MessageButtonPaginator.kt
+++ kord-extensions/src/main/kotlin/com/kotlindiscord/kord/extensions/pagination/MessageButtonPaginator.kt
@@ -97,6 +97,8 @@ public class MessageButtonPaginator(
                 this.components = mutableListOf()
             }
         }
+
+        runTimeoutCallbacks()
     }
 }
 
