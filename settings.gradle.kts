6fa3c2bf8ccabf5de81746640af930fb7b9c4f20
Gareth Coles <gdude2002@gmail.com>
2021-08-05 10:48:34 +0100

    Prep for 1.4.4-RC2

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,17 +2,17 @@ pluginManagement {
     repositories {
         google()
         gradlePluginPortal()
+    }
 
-        plugins {
-            // NOTE: UPDATE THIS IF YOU UPDATE THE LIBS.VERSIONS.TOML
+    plugins {
+        // NOTE: UPDATE THIS IF YOU UPDATE THE LIBS.VERSIONS.TOML
 
-            kotlin("jvm") version "1.5.10"
-            kotlin("plugin.serialization") version "1.5.10"
+        kotlin("jvm") version "1.5.10"
+        kotlin("plugin.serialization") version "1.5.10"
 
-            id("com.google.devtools.ksp") version "1.5.10-1.0.0-beta02"
-            id("io.gitlab.arturbosch.detekt") version "1.17.1"
-            id("org.jetbrains.dokka") version "1.4.10.2"
-        }
+        id("com.google.devtools.ksp") version "1.5.10-1.0.0-beta02"
+        id("io.gitlab.arturbosch.detekt") version "1.17.1"
+        id("org.jetbrains.dokka") version "1.4.10.2"
     }
 }
 
