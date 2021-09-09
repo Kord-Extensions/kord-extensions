91b4c490231fd298c421d82330c0f933eea00f98
Gareth Coles <gdude2002@gmail.com>
2021-09-09 09:38:43 +0100

    Fix snowflake-based top channel checks

--- kord-extensions/src/main/kotlin/com/kotlindiscord/kord/extensions/checks/TopChannelChecks.kt
+++ kord-extensions/src/main/kotlin/com/kotlindiscord/kord/extensions/checks/TopChannelChecks.kt
@@ -123,7 +123,7 @@ public suspend fun <T : Event> CheckContext<T>.inTopChannel(id: Snowflake) {
 
         fail()
     } else {
-        inChannel { channel }
+        inTopChannel { channel }
     }
 }
 
@@ -153,7 +153,7 @@ public suspend fun <T : Event> CheckContext<T>.notInTopChannel(id: Snowflake) {
 
         pass()
     } else {
-        notInChannel { channel }
+        notInTopChannel { channel }
     }
 }
 
