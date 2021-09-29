150d9078f6de52cf33abfa48a11c56f97ef0ca7b
Michael Rittmeister <michael@rittmeister.in>
2021-09-29 16:45:52 +0200

    Fix Gradle plugin versions

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
 }
 
 repositories {
+    google()
     gradlePluginPortal()
 }
 
@@ -10,8 +11,8 @@ dependencies {
     implementation(kotlin("gradle-plugin", version = "1.5.30"))
     implementation(kotlin("serialization", version = "1.5.30"))
     implementation("org.jetbrains.dokka", "dokka-gradle-plugin", "1.5.0")
-    implementation("org.jetbrains.kotlinx", "atomicfu-gradle-plugin", "0.16.1")
-    implementation("io.gitlab.arturbosch.detekt", "detekt-gradle-plugin", "1.18.1")
+    implementation("com.google.devtools.ksp", "com.google.devtools.ksp.gradle.plugin", "1.5.30-1.0.0-beta08")
+    implementation("io.gitlab.arturbosch.detekt", "detekt-gradle-plugin", "1.17.1")
     implementation(gradleApi())
     implementation(localGroovy())
 }
