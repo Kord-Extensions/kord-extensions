36439c98fcd6581b0cd2322f7d7c81cd19194f72
Michael Rittmeister <michael@rittmeister.in>
2021-09-29 18:36:36 +0200

    Fix dokka doc generation - Fix hardcoded module path - Fix extension not being found

--- buildSrc/src/main/kotlin/dokka-module.gradle.kts
+++ buildSrc/src/main/kotlin/dokka-module.gradle.kts
@@ -8,23 +8,30 @@ val dokkaModuleExtensionName = "dokkaModule"
 
 abstract class DokkaModuleExtension {
     abstract val moduleName: Property<String>
+    abstract val includes: ListProperty<String>
 }
 
 extensions.create<DokkaModuleExtension>(dokkaModuleExtensionName)
 
 tasks {
     afterEvaluate {
+        val projectDir = project.projectDir.relativeTo(rootProject.rootDir).toString()
         dokkaHtml {
-            val extension = extensions.getByName<DokkaModuleExtension>(dokkaModuleExtensionName)
-            moduleName.set(extension.moduleName.get())
+            val extension = project.extensions.getByName<DokkaModuleExtension>(dokkaModuleExtensionName)
+            extension.moduleName.orNull?.let {
+                moduleName.set(it)
+            }
 
             dokkaSourceSets {
                 configureEach {
                     includeNonPublic.set(false)
                     skipDeprecated.set(false)
-
-                    displayName.set(extension.moduleName.get())
-                    includes.from("packages.md")
+                    extension.moduleName.orNull?.let {
+                        displayName.set(it)
+                    }
+                    extension.includes.orNull?.let {
+                        includes.from(*it.toTypedArray())
+                    }
                     jdkVersion.set(8)
 
                     sourceLink {
@@ -33,7 +40,7 @@ tasks {
                         remoteUrl.set(
                             URL(
                                 "https://github.com/Kotlin-Discord/kord-extensions/" +
-                                    "tree/${getCurrentGitBranch()}/annotation-processor/src/main/kotlin"
+                                    "tree/${getCurrentGitBranch()}/${projectDir}/src/main/kotlin"
                             )
                         )
 
--- kord-extensions/build.gradle.kts
+++ kord-extensions/build.gradle.kts
@@ -50,3 +50,7 @@ val compileKotlin: KotlinCompile by tasks
 compileKotlin.kotlinOptions {
     languageVersion = "1.5"
 }
+
+dokkaModule {
+    includes.add("packages.md")
+}
