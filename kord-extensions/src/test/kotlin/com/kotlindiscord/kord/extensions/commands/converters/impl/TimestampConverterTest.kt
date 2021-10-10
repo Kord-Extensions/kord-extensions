d4ca7a0a7d161ef4ed3e79aee9c9f6108b93f370
AppleTheGolden <scotsbox@protonmail.com>
2021-10-10 18:37:05 +0200

    Fix compiling tests

--- kord-extensions/src/test/kotlin/com/kotlindiscord/kord/extensions/commands/converters/impl/TimestampConverterTest.kt
+++ kord-extensions/src/test/kotlin/com/kotlindiscord/kord/extensions/commands/converters/impl/TimestampConverterTest.kt
@@ -10,7 +10,7 @@ internal class TimestampConverterTest {
     @Test
     fun `timestamp without format`() {
         val timestamp = "<t:1420070400>" // 1st second of 2015
-        val parsed = TimestampConverter().parseFromString(timestamp)!!
+        val parsed = TimestampConverter.parseFromString(timestamp)!!
         assertEquals(Instant.fromEpochSeconds(1_420_070_400), parsed.instant)
         assertEquals(TimestampType.Default, parsed.format)
     }
@@ -18,7 +18,7 @@ internal class TimestampConverterTest {
     @Test
     fun `timestamp with format`() {
         val timestamp = "<t:1420070400:R>"
-        val parsed = TimestampConverter().parseFromString(timestamp)!!
+        val parsed = TimestampConverter.parseFromString(timestamp)!!
         assertEquals(Instant.fromEpochSeconds(1_420_070_400), parsed.instant)
         assertEquals(TimestampType.RelativeTime, parsed.format)
     }
@@ -26,14 +26,14 @@ internal class TimestampConverterTest {
     @Test
     fun `empty timestamp`() {
         val timestamp = "<t::>"
-        val parsed = TimestampConverter().parseFromString(timestamp)
+        val parsed = TimestampConverter.parseFromString(timestamp)
         assertNull(parsed)
     }
 
     @Test
     fun `timestamp with empty format`() {
         val timestamp = "<t:1420070400:>"
-        val parsed = TimestampConverter().parseFromString(timestamp)
+        val parsed = TimestampConverter.parseFromString(timestamp)
         assertNull(parsed)
     }
 }
