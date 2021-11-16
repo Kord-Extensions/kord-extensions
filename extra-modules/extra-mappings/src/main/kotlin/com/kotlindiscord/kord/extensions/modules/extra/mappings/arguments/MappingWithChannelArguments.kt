037f33bc5eba37a30f7ab131d666dc22f57c7db2
sschr15 <steven.sschr15@hotmail.com>
2021-11-16 10:44:09 -0600

    Detekt happiness

--- extra-modules/extra-mappings/src/main/kotlin/com/kotlindiscord/kord/extensions/modules/extra/mappings/arguments/MappingWithChannelArguments.kt
+++ extra-modules/extra-mappings/src/main/kotlin/com/kotlindiscord/kord/extensions/modules/extra/mappings/arguments/MappingWithChannelArguments.kt
@@ -3,6 +3,8 @@ package com.kotlindiscord.kord.extensions.modules.extra.mappings.arguments
 import com.kotlindiscord.kord.extensions.commands.application.slash.converters.ChoiceEnum
 import me.shedaniel.linkie.Namespace
 
+/** An argument base which provides an argument for a mapping channel. **/
+@Suppress("UndocumentedPublicProperty")
 abstract class MappingWithChannelArguments(namespace: Namespace) : MappingArguments(namespace) {
     abstract val channel: ChoiceEnum?
 }
