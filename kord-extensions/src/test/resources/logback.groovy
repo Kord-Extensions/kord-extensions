0cece53c34d78f36579231ccc7e4c3f0c51b21bd
Gareth Coles <gdude2002@gmail.com>
2021-12-28 22:07:27 +0000

    Well we couldn't commit it all at once could we, that'd be too easy, wouldn't it IJ

--- kord-extensions/src/test/resources/logback.groovy
+++ kord-extensions/src/test/resources/logback.groovy
@@ -6,8 +6,6 @@
 
 
 import ch.qos.logback.classic.encoder.PatternLayoutEncoder
-import ch.qos.logback.core.ConsoleAppender
-import ch.qos.logback.core.FileAppender
 import ch.qos.logback.core.joran.spi.ConsoleTarget
 
 def environment = System.getenv().getOrDefault("ENVIRONMENT", "production")
