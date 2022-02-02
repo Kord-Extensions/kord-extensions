9c3c0abe195245b89a91f73f3cb52cdd9ac35d0b
Gareth Coles <gdude2002@gmail.com>
2022-02-02 20:47:13 +0000

    [#132] Convenience function to get a thread's parent message

--- kord-extensions/src/main/kotlin/com/kotlindiscord/kord/extensions/utils/_Channel.kt
+++ kord-extensions/src/main/kotlin/com/kotlindiscord/kord/extensions/utils/_Channel.kt
@@ -10,6 +10,7 @@ import dev.kord.common.entity.Permissions
 import dev.kord.common.entity.Snowflake
 import dev.kord.core.behavior.UserBehavior
 import dev.kord.core.behavior.channel.createWebhook
+import dev.kord.core.entity.Message
 import dev.kord.core.entity.Webhook
 import dev.kord.core.entity.channel.GuildChannel
 import dev.kord.core.entity.channel.TopGuildChannel
@@ -77,3 +78,14 @@ public suspend fun GuildChannel.permissionsForMember(memberId: Snowflake): Permi
  */
 public suspend fun GuildChannel.permissionsForMember(user: UserBehavior): Permissions =
     permissionsForMember(user.id)
+
+/**
+ * Convenience function that returns the thread's parent message, if it was created from one.
+ *
+ * If it wasn't, or the parent channel can't be found, this function returns `null`.
+ */
+public suspend fun ThreadChannel.getParentMessage(): Message? {
+    val parentChannel = getParentOrNull() ?: return null
+
+    return parentChannel.getMessageOrNull(this.id)
+}
