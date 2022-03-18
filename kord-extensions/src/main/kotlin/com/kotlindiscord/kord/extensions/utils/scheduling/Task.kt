24db3e699596ba66f38f31d8bc8c599e470d05fb
Gareth Coles <gdude2002@gmail.com>
2022-03-18 13:30:40 +0000

    Tasks: Address memory leak

--- kord-extensions/src/main/kotlin/com/kotlindiscord/kord/extensions/utils/scheduling/Task.kt
+++ kord-extensions/src/main/kotlin/com/kotlindiscord/kord/extensions/utils/scheduling/Task.kt
@@ -52,6 +52,13 @@ public open class Task(
     protected lateinit var started: TimeMark
     protected val sentry: SentryAdapter by inject()
 
+    /**
+     * Number of times this task has been executed.
+     *
+     * If a ULong is too small for this... wyd?
+     */
+    public var executions: ULong = 0UL
+
     /** Whether this task is currently running - that is, waiting until it's time to execute the [callback]. **/
     public val running: Boolean get() = job != null
 
@@ -64,15 +71,18 @@ public open class Task(
 
         started = TimeSource.Monotonic.markNow()
 
-        sentryContext.breadcrumb(BreadcrumbType.Info) {
-            val now = Clock.System.now().toLocalDateTime(TimeZone.UTC)
+        if (executions == 0UL) {
+            sentryContext.breadcrumb(BreadcrumbType.Info) {
+                val now = Clock.System.now().toLocalDateTime(TimeZone.UTC)
 
-            message = "Starting task: waiting for configured delay to pass"
+                message = "Starting task: waiting for configured delay to pass"
 
-            data["delay"] = duration.toIsoString()
-            data["name"] = name
-            data["now"] = now.toString()
-            data["pollingSeconds"] = pollingSeconds
+                data["delay"] = duration.toIsoString()
+                data["name"] = name
+                data["now"] = now.toString()
+                data["pollingSeconds"] = pollingSeconds
+                data["repeating"] = repeat
+            }
         }
 
         job = coroutineScope.launch {
@@ -81,12 +91,14 @@ public open class Task(
                 delay(pollingSeconds * 1000)
             }
 
-            sentryContext.breadcrumb(BreadcrumbType.Info) {
-                val now = Clock.System.now().toLocalDateTime(TimeZone.UTC)
+            if (executions == 0UL) {
+                sentryContext.breadcrumb(BreadcrumbType.Info) {
+                    val now = Clock.System.now().toLocalDateTime(TimeZone.UTC)
 
-                message = "Delay has passed, executing task"
+                    message = "Delay has passed, executing task (for the first time)"
 
-                data["now"] = now.toString()
+                    data["now"] = now.toString()
+                }
             }
 
             @Suppress("TooGenericExceptionCaught")
@@ -100,10 +112,13 @@ public open class Task(
 
                     if (sentry.enabled) {
                         sentryContext.captureException(t, "Error running scheduled callback") {
+                            setExtra("executions", executions.toString())
                             tag("task", name)
                         }
                     }
                 }
+            } finally {
+                executions += 1UL
             }
 
             if (!repeat) {
