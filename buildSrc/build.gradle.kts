a92730e084f6a723792c9e4bcb6e261bcafe8df8
NoComment1105 <nocomment1105@outlook.com>
2022-04-11 11:55:10 +0100

    Update ksp

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -13,7 +13,7 @@ dependencies {
 
     implementation("gradle.plugin.org.cadixdev.gradle", "licenser", "0.6.1")
     implementation("com.github.jakemarsden", "git-hooks-gradle-plugin", "0.0.2")
-    implementation("com.google.devtools.ksp", "com.google.devtools.ksp.gradle.plugin", "1.6.20-1.0.4")
+    implementation("com.google.devtools.ksp", "com.google.devtools.ksp.gradle.plugin", "1.6.20-1.0.5")
     implementation("io.gitlab.arturbosch.detekt", "detekt-gradle-plugin", "1.19.0")
     implementation("org.jetbrains.dokka", "dokka-gradle-plugin", "1.6.10")
 
