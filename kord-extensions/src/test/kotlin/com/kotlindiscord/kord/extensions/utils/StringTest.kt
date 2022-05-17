d11a8e69006f369e7b38ffb3d3502cf1289224dd
Gareth Coles <gdude2002@gmail.com>
2022-05-17 18:07:27 +0100

    🚧 String.toReaction now handles custom emoji

--- kord-extensions/src/test/kotlin/com/kotlindiscord/kord/extensions/utils/StringTest.kt
+++ kord-extensions/src/test/kotlin/com/kotlindiscord/kord/extensions/utils/StringTest.kt
@@ -6,10 +6,13 @@
 
 package com.kotlindiscord.kord.extensions.utils
 
+import dev.kord.common.entity.Snowflake
+import dev.kord.core.entity.ReactionEmoji
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.parallel.Execution
 import org.junit.jupiter.api.parallel.ExecutionMode
+import kotlin.test.assertNotNull
 
 /**
  * Tests for [String] extension functions.
@@ -25,6 +28,32 @@ class StringTest {
         val unicode = "❤"
         val reaction = unicode.toReaction()
         assertEquals(unicode, reaction.name)
+    }
+
+    /**
+     * Check that `.toReaction()` for custom emojis transforms the string correctly.
+     */
+    @Suppress("UnderscoresInNumericLiterals", "MagicNumber")
+    @Test
+    @Execution(ExecutionMode.CONCURRENT)
+    fun `custom to reaction`() {
+        val staticString = "<:cozy:859058952393457694>"
+        val staticReaction = staticString.toReaction() as? ReactionEmoji.Custom
+
+        assertNotNull(staticReaction, "Returned ReactionEmoji object must be a custom emoji")
+
+        assertEquals(staticReaction.name, "cozy")
+        assertEquals(staticReaction.id, Snowflake(859058952393457694))
+        assertEquals(staticReaction.isAnimated, false)
+
+        val animatedString = "<a:ablobzerogravity:614485981021601812>"
+        val animatedReaction = animatedString.toReaction() as? ReactionEmoji.Custom
+
+        assertNotNull(animatedReaction, "Returned ReactionEmoji object must be a custom emoji")
+
+        assertEquals(animatedReaction.name, "ablobzerogravity")
+        assertEquals(animatedReaction.id, Snowflake(614485981021601812))
+        assertEquals(animatedReaction.isAnimated, true)
     }
 
     /**
