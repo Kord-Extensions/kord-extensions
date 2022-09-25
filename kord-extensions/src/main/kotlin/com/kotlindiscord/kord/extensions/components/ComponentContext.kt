c575026f62d1ce37f7f7d6eb9847c932a3f64bc3
Allan Galarza <allan.galarza@gmail.com>
2022-09-25 10:45:59 -0700

    Added translate overloads for ComponentContext

--- kord-extensions/src/main/kotlin/com/kotlindiscord/kord/extensions/components/ComponentContext.kt
+++ kord-extensions/src/main/kotlin/com/kotlindiscord/kord/extensions/components/ComponentContext.kt
@@ -156,6 +156,30 @@ public abstract class ComponentContext<E : ComponentInteractionCreateEvent>(
     )
 
     /**
+     * Given a translation key and bundle name, return the translation for the locale provided by the bot's configured
+     * locale resolvers.
+     */
+    public suspend fun translate(
+        key: String,
+        bundleName: String?,
+        replacements: Map<String, Any?>
+    ): String {
+        val locale = getLocale()
+
+        return translationsProvider.translate(key, locale, bundleName, replacements)
+    }
+
+    /**
+     * Given a translation key and possible replacements, return the translation for the given locale in the
+     * component's configured bundle, for the locale provided by the bot's configured locale resolvers.
+     */
+    public suspend fun translate(key: String, replacements: Map<String, Any?>): String = translate(
+        key,
+        component.bundle,
+        replacements
+    )
+
+    /**
      * @param breadcrumb breadcrumb data will be modified to add the component context information
      */
     public suspend fun addContextDataToBreadcrumb(breadcrumb: Breadcrumb) {
