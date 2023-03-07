3f3ba9af9d3ff82f4b28c433dad782a827a20c92
Gareth Coles <gdude2002@gmail.com>
2023-03-07 15:51:57 +0000

    Apparently IJ just can't help itself

--- kord-extensions/src/main/kotlin/com/kotlindiscord/kord/extensions/checks/CheckUtils.kt
+++ kord-extensions/src/main/kotlin/com/kotlindiscord/kord/extensions/checks/CheckUtils.kt
@@ -4,7 +4,7 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
-@file:OptIn(KordUnsafe::class, KordExperimental::class, KordUnsafe::class, KordUnsafe::class, KordUnsafe::class)
+@file:OptIn(KordUnsafe::class, KordExperimental::class)
 
 package com.kotlindiscord.kord.extensions.checks
 
