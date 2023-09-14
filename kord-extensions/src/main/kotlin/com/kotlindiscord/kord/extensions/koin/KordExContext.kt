3b0d34db4eac73fe769a102b99646ccdd692d7f2
Gareth Coles <gdude2002@gmail.com>
2023-09-14 13:20:21 +0100

    Dependency updates

--- kord-extensions/src/main/kotlin/com/kotlindiscord/kord/extensions/koin/KordExContext.kt
+++ kord-extensions/src/main/kotlin/com/kotlindiscord/kord/extensions/koin/KordExContext.kt
@@ -8,6 +8,7 @@ package com.kotlindiscord.kord.extensions.koin
 
 import org.koin.core.Koin
 import org.koin.core.KoinApplication
+import org.koin.core.context.GlobalContext
 import org.koin.core.context.KoinContext
 import org.koin.core.error.KoinAppAlreadyStartedException
 import org.koin.core.module.Module
@@ -23,111 +24,103 @@ import org.koin.dsl.KoinAppDeclaration
  * @see org.koin.core.context.GlobalContext
  */
 public object KordExContext : KoinContext {
-    /** The current [Koin] instance. */
-    private var koin: Koin? = null
+	/** The current [Koin] instance. */
+	private var koin: Koin? = null
 
-    /** The current [KoinApplication]. */
-    private var koinApp: KoinApplication? = null
+	/** The current [KoinApplication]. */
+	private var koinApp: KoinApplication? = null
 
-    /**
-     * Gets the [Koin] instance.
-     *
-     * @throws IllegalStateException [KoinApplication] has not yet been started.
-     */
-    override fun get(): Koin = koin ?: error("KordEx KoinApplication has not been started")
+	/**
+	 * Gets the [Koin] instance.
+	 *
+	 * @throws IllegalStateException [KoinApplication] has not yet been started.
+	 */
+	override fun get(): Koin = koin ?: error("KordEx KoinApplication has not been started")
 
-    /** Gets the [Koin] instance or null if the [KoinApplication] has not yet been started. */
-    override fun getOrNull(): Koin? = koin
+	/** Gets the [Koin] instance or null if the [KoinApplication] has not yet been started. */
+	override fun getOrNull(): Koin? = koin
 
-    /** Gets the [KoinApplication] or null if the [KoinApplication] has not yet been started. */
-    public fun getKoinApplicationOrNull(): KoinApplication? = koinApp
+	/** Gets the [KoinApplication] or null if the [KoinApplication] has not yet been started. */
+	public fun getKoinApplicationOrNull(): KoinApplication? = koinApp
 
-    /**
-     * Registers a [KoinApplication] to as the current one for this context.
-     *
-     * @param koinApplication The application to registers.
-     *
-     * @throws KoinAppAlreadyStartedException The [KoinApplication] has already been instantiated.
-     */
-    private fun register(koinApplication: KoinApplication) {
-        if (koin != null) {
-            throw KoinAppAlreadyStartedException("KordEx Koin Application has already been started")
-        }
+	/**
+	 * Registers a [KoinApplication] to as the current one for this context.
+	 *
+	 * @param koinApplication The application to registers.
+	 *
+	 * @throws KoinAppAlreadyStartedException The [KoinApplication] has already been instantiated.
+	 */
+	private fun register(koinApplication: KoinApplication) {
+		if (koin != null) {
+			throw KoinAppAlreadyStartedException("KordEx Koin Application has already been started")
+		}
 
-        koinApp = koinApplication
-        koin = koinApplication.koin
-    }
+		koinApp = koinApplication
+		koin = koinApplication.koin
+	}
 
-    /** Closes and removes the current [Koin] instance. */
-    override fun stopKoin(): Unit = synchronized(this) {
-        koin?.close()
-        koin = null
-    }
+	/** Closes and removes the current [Koin] instance. */
+	override fun stopKoin(): Unit = synchronized(this) {
+		koin?.close()
+		koin = null
+	}
 
-    /**
-     * Starts using the provided [KoinApplication] as the current one for this context.
-     *
-     * @param koinApplication The application to start with.
-     *
-     * @throws KoinAppAlreadyStartedException The [KoinApplication] has already been instantiated.
-     */
-    override fun startKoin(koinApplication: KoinApplication): KoinApplication = synchronized(this) {
-        register(koinApplication)
-        koinApplication.createEagerInstances()
+	/**
+	 * Starts using the provided [KoinApplication] as the current one for this context.
+	 *
+	 * @param koinApplication The application to start with.
+	 *
+	 * @throws KoinAppAlreadyStartedException The [KoinApplication] has already been instantiated.
+	 */
+	override fun startKoin(koinApplication: KoinApplication): KoinApplication = synchronized(this) {
+		register(koinApplication)
+		koinApplication.createEagerInstances()
 
-        return koinApplication
-    }
+		return koinApplication
+	}
 
-    /**
-     * Starts using the provided [KoinAppDeclaration] to create the [KoinApplication] for this context.
-     *
-     * @param appDeclaration The application declaration to start with.
-     *
-     * @throws KoinAppAlreadyStartedException The [KoinApplication] has already been instantiated.
-     */
-    override fun startKoin(appDeclaration: KoinAppDeclaration): KoinApplication = synchronized(this) {
-        val koinApplication = KoinApplication.init()
+	/**
+	 * Starts using the provided [KoinAppDeclaration] to create the [KoinApplication] for this context.
+	 *
+	 * @param appDeclaration The application declaration to start with.
+	 *
+	 * @throws KoinAppAlreadyStartedException The [KoinApplication] has already been instantiated.
+	 */
+	override fun startKoin(appDeclaration: KoinAppDeclaration): KoinApplication = synchronized(this) {
+		val koinApplication = KoinApplication.init()
 
-        register(koinApplication)
-        appDeclaration(koinApplication)
-        koinApplication.createEagerInstances()
+		register(koinApplication)
+		appDeclaration(koinApplication)
+		koinApplication.createEagerInstances()
 
-        return koinApplication
-    }
+		return koinApplication
+	}
 
-    /**
-     * Loads a module into the [Koin] instance.
-     *
-     * @param module The module to load.
-     */
-    override fun loadKoinModules(module: Module): Unit = synchronized(this) {
-        get().loadModules(listOf(module))
-    }
+	/** Mirroring the global Koin instance implementation. **/
+	override fun loadKoinModules(module: Module, createEagerInstances: Boolean): Unit = synchronized(this) {
+		GlobalContext.get().loadModules(listOf(module), createEagerInstances = createEagerInstances)
+	}
 
-    /**
-     * Loads modules into the [Koin] instance.
-     *
-     * @param modules The modules to load.
-     */
-    override fun loadKoinModules(modules: List<Module>): Unit = synchronized(this) {
-        get().loadModules(modules)
-    }
+	/** Mirroring the global Koin instance implementation. **/
+	override fun loadKoinModules(modules: List<Module>, createEagerInstances: Boolean): Unit = synchronized(this) {
+		GlobalContext.get().loadModules(modules, createEagerInstances = createEagerInstances)
+	}
 
-    /**
-     * Unloads a module from the [Koin] instance.
-     *
-     * @param module The module to unload.
-     */
-    override fun unloadKoinModules(module: Module): Unit = synchronized(this) {
-        get().unloadModules(listOf(module))
-    }
+	/**
+	 * Unloads a module from the [Koin] instance.
+	 *
+	 * @param module The module to unload.
+	 */
+	override fun unloadKoinModules(module: Module): Unit = synchronized(this) {
+		get().unloadModules(listOf(module))
+	}
 
-    /**
-     * Unloads modules from the [Koin] instance.
-     *
-     * @param modules The modules to unload.
-     */
-    override fun unloadKoinModules(modules: List<Module>): Unit = synchronized(this) {
-        get().unloadModules(modules)
-    }
+	/**
+	 * Unloads modules from the [Koin] instance.
+	 *
+	 * @param modules The modules to unload.
+	 */
+	override fun unloadKoinModules(modules: List<Module>): Unit = synchronized(this) {
+		get().unloadModules(modules)
+	}
 }
