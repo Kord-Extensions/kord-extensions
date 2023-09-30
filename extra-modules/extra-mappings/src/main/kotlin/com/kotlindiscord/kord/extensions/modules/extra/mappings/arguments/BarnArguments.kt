013533b76b89be33c9ff35628a024605bab4d89a
sschr15 <steven.sschr15@hotmail.com>
2023-09-30 12:45:05 -0500

    Update to latest Linkie + 3 new namespaces (#243)

---
+++ extra-modules/extra-mappings/src/main/kotlin/com/kotlindiscord/kord/extensions/modules/extra/mappings/arguments/BarnArguments.kt
@@ -0,0 +1,20 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+package com.kotlindiscord.kord.extensions.modules.extra.mappings.arguments
+
+import com.kotlindiscord.kord.extensions.commands.converters.impl.defaultingBoolean
+import me.shedaniel.linkie.namespaces.BarnNamespace
+
+/** Arguments for Barn mappings lookup commands. **/
+@Suppress("UndocumentedPublicProperty")
+class BarnArguments : MappingArguments(BarnNamespace), IntermediaryMappable {
+    override val mapDescriptors by defaultingBoolean {
+        name = "map-descriptor"
+        description = "Whether to map field/method descriptors to named instead of intermediary"
+        defaultValue = true
+    }
+}
